4db6d4616951fa49a3c26e3ac789d868c343f93c
nicolas <nicolas0978pzzz@gmail.com>
2026-06-06 22:13:32 -0500

    Eventos JS con boton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Empezando a usar JavaScript");
+
+// Seleccionamos el h1 (Cafe +) y el botón que acabamos de agregar
+const titulo = document.querySelector("#titulo");
+const boton = document.querySelector("#btnCambiar");
+
+// Escuchamos el clic para cambiar el texto del título
+boton.addEventListener('click', () => {
+    titulo.textContent = '¡Bienvenido a Cafe +!';
+});
